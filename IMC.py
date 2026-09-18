3f2c93dfa5827d6261a32aab3307ffa2ff3b7852
Mohamed amine Trad <mohamedaminetrad@MacBook-Air-de-Mohamed.local>
2026-09-18 18:50:08 +0200

    MAJ IMC.py

--- IMC.py
+++ IMC.py
@@ -1,5 +1,10 @@
 #This program calculates IMC from weight and height
 weight = input ('Enter you weigth in kg = ')
 height = input ('Enter your height in m = ')
-imc = float(weight) / (float(height) ** 2)
-print("Your IMC is: " + str(imc))
+if float(height) <= 0:
+    print("Height must be greater than zero.")
+elif float(weight) <= 0:
+    print("Weight must be greater than zero.")
+else:
+    imc = float(weight) / (float(height) ** 2)
+    print("Your IMC is: " + str(imc))
